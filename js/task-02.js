f0fbfa3f97ebe97e101aaf4213b53daaaf19ac14
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-07-24 17:24:44 +0300

    task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -4,5 +4,26 @@ const ingredients = [
   'Garlic',
   'Tomatos',
   'Herbs',
-  'Condiments',
+  'Condiments',  
 ];
+
+const ingredientsRef = document.querySelector('#ingredients')
+
+
+function createIngredientsItemsMarkup(ingredients) {
+  
+  const ingredientsItem = [];
+
+  for (let ingredient of ingredients) {
+
+    const ingredientItemRef = document.createElement('li');
+
+    ingredientItemRef.textContent = ingredient;
+    ingredientItemRef.classList.add('item');
+    ingredientsItem.push(ingredientItemRef);      
+  }
+
+  ingredientsRef.append(...ingredientsItem);
+}
+
+createIngredientsItemsMarkup(ingredients)
