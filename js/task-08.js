5dc19e36abcd744b7aa6688c81a52098d6b7356d
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-07-24 23:50:39 +0300

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector('.login-form');
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new formData(event.currentTarget);
+    const element = event.currentTarget.elements;
+
+    
+    if (element.email.value === '' || element.password.value === '') {
+        alert("Необхідно заповнити усі поля");
+    }
+   
+
+    return formData;
+}
 
