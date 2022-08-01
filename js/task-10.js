df14874816f07a68626f5ff1a2e32a55c7fee6c6
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-08-02 01:26:05 +0300

    task 10 fixed

--- js/task-10.js
+++ js/task-10.js
@@ -11,43 +11,49 @@ createBtn.addEventListener('click', onCreateBtnClick);
 destroyBtn.addEventListener('click', onDestroyBtnClick);
 
 
-function createBoxes(amount) {
+function onCreateBtnClick(amount) {
   const collectionElements = [];
   amount = inputRef.value;
   
-    for (let i = 1; i <= amount; i += 1) {
-      collectionElements.push(`<div></div>`)
-    }
+  for (let i = 1; i <= amount; i += 1) {
+    collectionElements.push('<div></div>')      
+  }  
 
-    return collectionElements.join('');  
-}
+  containerRef.insertAdjacentHTML("beforeend", collectionElements.join(''));
   
-function onCreateBtnClick() {
-  
-  containerRef.insertAdjacentHTML("beforeend", createBoxes());
- 
-  let elSize = 20;
-  
+  for (let i = 0; i < amount; i++) {    
+    containerRef.children[(containerRef.children.length - 1) - i].style.backgroundColor = getRandomHexColor();
+  }
+
+  let elSize = 20; 
+    
   for (const child of containerRef.children) {
     elSize += 10;    
     
-    child.style.backgroundColor = getRandomHexColor();
     child.style.width = elSize + 'px';
-    child.style.height = elSize + 'px';
-  }
+    child.style.height = elSize + 'px';    
+  }  
 }
 
-function onDestroyBtnClick(amount) {
 
-  amount = inputRef.value;
+function onDestroyBtnClick() {
 
-  if (amount > containerRef.children.length) {
-    containerRef.innerHTML = '';
-    return;
-  }
+  inputRef.value = '';
 
-  for (let i = 0; i < amount; i++) {
-    containerRef.lastElementChild.remove();
-  } 
+  containerRef.innerHTML = '';  
 }
+
+// function onDestroyBtnClick(amount) {
+
+//   amount = inputRef.value;
+
+//   if (amount > containerRef.children.length) {
+//     containerRef.innerHTML = '';
+//     return;
+//   }
+
+//   for (let i = 0; i < amount; i++) {
+//     containerRef.lastElementChild.remove();
+//   } 
+// }
 
